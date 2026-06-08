df88b614b28b91a75b88c52afcadff52015b0117
Michael <mg6090@princeton.edu>
2026-06-08 15:12:49 -0400

    start coding scenario 2 framework

--- scenario2MultiParty.py
+++ scenario2MultiParty.py
@@ -1,22 +1,34 @@
-# DAG FOR SCENARIO 3 (INDEPENDENT)
-
 import networkx as nx
+import random 
 
 #==================================================================
 # CONSTANTS (INPUT DATA AND PARAMETERS)
 #==================================================================
 
-# dictionary for CCS projects (capture + transport + storage)
-CCS = {
+# dictionary for capture projects
+CAPTURE = {
     "projC1": {"definition": 12, "approval": 24, "construction": 48},
     "projC2": {"definition": 30, "approval": 48, "construction": 60},
     "projC3": {"definition": 24, "approval": 24, "construction": 36},
     "projC4": {"definition": 16, "approval": 32, "construction": 50}
 }
 
+# dictionary for TS projects
+TS = {
+    "projS1": {"definition": 48, "approval": 36, "construction": 72},
+    # "projS2": {"definition": 60, "approval": 36, "construction": 96},
+    # "projS3": {"definition": 48, "approval": 24, "construction": 60}
+}
+
 # stages of projects
 STAGES = ["definition", "approval", "construction"]
 STAGES4 = ["definition", "approval", "construction", "commission"]
+
+# fraction split
+FRAC_SPLIT = (0.2, 0.8)
+
+# seed for shuffling before frac_split 
+SEED = 42
 
 #==================================================================
 # BUILDING THE DAG — ADDING NODES
@@ -45,9 +57,13 @@ def projGraph():
                 EF = 0.0
             )
 
-    # add nodes for CCS projects
-    for pid, stages in CCS.items():
-        add_project(pid, stages, "CCS")
+    # add nodes for capture projects
+    for pid, stages in CAPTURE.items():
+        add_project(pid, stages, "Capture")
+    
+    # add nodes for TS projects
+    for pid, stages in TS.items():
+        add_project(pid, stages, 'TS')
     
     return G
 
@@ -69,8 +85,12 @@ def projEdges(G: nx.DiGraph):
         for a, b in zip(STAGES[:-1], STAGES[1:]):
             G.add_edge((pid, a), (pid, b))
 
-    # adding intra-project edges for CCS projects
-    for pid in CCS.keys():
+    # adding intra-project edges for capture projects
+    for pid in CAPTURE.keys():
+        graphEdges(G, pid)
+
+    # adding intra-project edges for TS projects 
+    for pid in TS.keys():
         graphEdges(G, pid)
 
 #==================================================================
@@ -96,6 +116,39 @@ def CPM(G: nx.DiGraph):
         G.nodes[node]["EF"] = updated_ES + G.nodes[node]["duration"] 
 
 #==================================================================
+# BUILDING THE DAG — ADDING INTER-PROJECT EDGES
+#==================================================================
+def fracSplit():
+        '''
+        Description: helper method that splits capture into two batches based on risk aversion level
+        Returns: tuple of ([approval pids], [construction pids])
+        '''
+        # number of capture projects in approval batch
+        approv_num = round(len(CAPTURE)*FRAC_SPLIT[0])
+        
+        pids = [key for key in CAPTURE.keys()]
+        
+        # seeding and introducing randomness
+        rng = random.Random(SEED)
+        rng.shuffle(pids)
+
+        # cuts the list of pids in two for the two sets (go ahead at approval and construction)
+        return pids[:approv_num], pids[approv_num:]
+
+def projInterdep(G: nx.DiGraph):
+    '''
+    Description: adds inter-project edges to G
+    Args: G
+    '''
+
+    # loop through projects
+    approval, construction = fracSplit()
+    for pid in approval:
+        G.add_edge(("projS1", "approval"), (pid, "construction"))
+    for pid in construction:
+        G.add_edge(("projS1", "construction"), (pid, "construction"))
+
+#==================================================================
 # RUNNING THE MODEL
 #==================================================================
 
@@ -103,12 +156,14 @@ def buildmodel():
     '''
     Description: runs the process 1) building graph and add nodes 
     -> 2) add intra-project edges
-    -> 3) run CPM
+    -> 3) add inter-project edges
+    -> 4) run CPM
     Returns: G
     '''
 
     G = projGraph()
     projEdges(G)
+    projInterdep(G)
     CPM(G)
 
     return G
@@ -121,6 +176,11 @@ G = buildmodel()
 
 # checking that G is a DAG
 print("Checking if G is DAG: " + str(nx.is_directed_acyclic_graph(G)))
+
+# inspecting the outputs of fracSplit
+approval, construction = fracSplit()
+print("Approval batch: ", approval)
+print("Construction batch: ", construction)
 
 # inspecting the nodes of G 
 print("Printing out nodes of the graph: ")
