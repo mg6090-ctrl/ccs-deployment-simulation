c00d45c2c3d479b355c3e9e75f7665ac565e7a88
Michael <mg6090@princeton.edu>
2026-06-09 13:12:33 -0400

    started attrition functions

--- scenario2MultiParty.py
+++ scenario2MultiParty.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import random 
+import numpy as np
 
 #==================================================================
 # CONSTANTS (INPUT DATA AND PARAMETERS)
@@ -14,7 +15,7 @@ CAPTURE = {
     "projC1": {"definition": 12, "approval": 24, "construction": 48},
     "projC2": {"definition": 30, "approval": 48, "construction": 60},
     "projC3": {"definition": 24, "approval": 24, "construction": 36},
-    "projC4": {"definition": 16, "approval": 32, "construction": 50}
+    # "projC4": {"definition": 16, "approval": 32, "construction": 50}
 }
 
 # dictionary for TS projects
@@ -33,6 +34,12 @@ CLUSTERS = {
 
 # seed for shuffling before frac_split 
 SEED = 42
+
+# constants for the abandonment function
+BASE_RATE = 0.2
+MAX_RATE = 0.8
+CAPTURE_TOLERANCE = 18
+TS_TOLERANCE = 12
 
 #==================================================================
 # BUILDING THE DAG — ADDING NODES
@@ -173,6 +180,124 @@ def CPM(G: nx.DiGraph):
         G.nodes[node]["EF"] = updated_ES + G.nodes[node]["duration"]
 
 #==================================================================
+# DELAYS AND ABANDONMENT
+#==================================================================
+
+def make_base_graph():
+    '''
+    Description: builds intra-project DAG without joint nodes
+    Returns: G_indep
+    '''
+
+    # helper method for drawing project nodes only (no joint nodes)
+    def intra_nodes():
+        # make one large graph
+        G = nx.DiGraph()
+
+        for ts, captures in CLUSTERS.items():
+            # cluster number is determined by ts project
+            cluster =  ts + " cluster"
+
+            # add ts nodes
+            for stage, dur in TS[ts].items():
+                G.add_node(
+                    (ts, stage),
+                    duration = dur,
+                    stage = stage,
+                    cluster = cluster,
+                    tech = "TS",
+                    ES = 0.0,
+                    EF = 0.0
+                )
+
+            for capture in captures:
+                # add capture nodes
+                for stage, dur in CAPTURE[capture].items():
+                    G.add_node(
+                        (capture, stage),
+                        duration = dur,
+                        stage = stage,
+                        cluster = cluster,
+                        tech = "Capture",
+                        ES = 0.0,
+                        EF = 0.0
+                    )
+        return G
+
+    # helper method for drawing intra-project edges only
+    def intra_edges(G: nx.DiGraph):
+        def graphEdges(G, pid):
+            for a, b in zip(STAGES[:-1], STAGES[1:]):
+                G.add_edge((pid, a), (pid, b))
+
+        for pid in CAPTURE.keys():
+            graphEdges(G, pid)
+
+        for pid in TS.keys():
+            graphEdges(G, pid)
+
+    # make a NEW graph without interdependent edges to evaluate baseline ES/EF
+    G_indep = intra_nodes()
+    intra_edges(G_indep)
+
+    return G_indep
+
+def slip_compute(node):
+    '''
+    Description: returns slip time of a given node
+    Args: node (project, stage); expectation: do not enter a joint node because we need to get 
+    this slip time for the TS and capture projects; it does not make sense to calculate it for 
+    a joint node because joint nodes do not exist in the base graph
+    Returns: slip time 
+    '''
+
+    # make DAG with no joint nodes (no interdependencies) and run CPM
+    G_base = make_base_graph()
+    CPM(G_base)
+
+    # make DAG with interdependencies (actual) and run CPM
+    G_actual = projGraph()
+    projEdges(G_actual)
+    CPM(G_actual)
+
+    base_EF = G_base.nodes[node]["EF"]
+
+    # corresponding joint node for this stage in the graph with interdependencies is the next node
+    # if (capture, definition) -> want EF of def joint node
+    # if (capture, approval) -> want EF of app joint node
+    # if (capture, cons) -> want EF of commissioning joint node 
+    actual_EF = G_actual.nodes[list(G_actual.successors(node))[0]]["EF"]
+
+    # actual_EF = G_actual.node[node]["EF"]
+    slip_time = actual_EF - base_EF
+    
+    return base_EF, actual_EF, slip_time
+
+def calculate_attrition_probability(delay, tech):
+    '''
+    Description: returns attrition probability based on delay and tech
+    Principle: slip past per-party tolerance baseline
+    Args: delay (calculated from slip_compute) and tech
+    Returns: probability
+    '''
+    if tech == "capture":
+        if delay < CAPTURE_TOLERANCE:
+            return BASE_RATE
+        else:
+            delay_factor = delay / CAPTURE_TOLERANCE
+            return min(MAX_RATE, BASE_RATE * (1 + np.exp(delay_factor)))
+
+    if tech == "TS":
+        if delay < TS_TOLERANCE:
+            return BASE_RATE
+        else:
+            delay_factor = delay / TS_TOLERANCE
+            return min(MAX_RATE, BASE_RATE * (1 + np.exp(delay_factor)))
+
+def apply_attrition():
+    return
+
+#==================================================================
 # RUNNING THE MODEL
 #==================================================================
 
@@ -195,12 +320,18 @@ def buildmodel():
 # MAIN (EXECUTION)
 #==================================================================
 
-G = buildmodel()
+# G = buildmodel()
 
 # checking that G is a DAG
-print("Checking if G is DAG: " + str(nx.is_directed_acyclic_graph(G)))
+# print("Checking if G is DAG: " + str(nx.is_directed_acyclic_graph(G)))
 
 # inspecting the nodes of G 
-print("Printing out nodes of the graph: ")
-for n in G.nodes:
-    print(n, G.nodes[n])
+# print("Printing out nodes of the graph: ")
+# for n in G.nodes:
+    # print(n, G.nodes[n])
+
+# testing slip_compute
+print(slip_compute(("projC1", "definition")))
+print(slip_compute(("projC1", "approval")))
+print(slip_compute(("projS1", "definition")))
+print(slip_compute(("projS1", "approval")))
