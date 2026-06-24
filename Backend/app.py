33881d953df8d92962fef5e36a442e582eeb269a
Michael <mg6090@princeton.edu>
2026-06-24 11:23:19 -0400

    create route for s1 simulation

--- Backend/app.py
+++ Backend/app.py
@@ -4,25 +4,26 @@ from models import scenario_2_final_refactored as s2
 
 app = Flask(__name__)
 
-@app.route('/api/run-s1', methods=['GET'])
+
+
+@app.route('/api/run-s1', methods=['GET', 'POST'])
 def double_number():
-    data = request.get_json()
+    data = request.get_json(silent = True) or {}
     
-    n_reps = data.get('n_reps')
-    sampling = data.get('sampling')
-    frac_split = data.get('frac_split')
-    base_rate = data.get('base_rate')
-    replication_seed = data.get('replication_seed')
-    clusters = data.get('clusters')
-    caps = data.get('caps')
-    caps_vol = data.get('caps_vol')
-    trans = data.get('trans')
-    trans_vol = data.get('trans_vol')
-    stor = data.get('stor')
-    stor_vol = data.get('stor_vol')
+    n_reps = data.get('n_reps', 10)
+    sampling = data.get('sampling', True)
+    frac_split = data.get('frac_split', s1.FRAC_SPLIT)
+    base_rate = data.get('base_rate', s1.BASE_RATE)
+    replication_seed = data.get('replication_seed', 0)
+    clusters = data.get('clusters', s1.CLUSTERS)
+    caps = data.get('caps', s1.CAPTURE)
+    caps_vol = data.get('caps_vol', s1.CAPTURE_VOLUMES)
+    trans = data.get('trans', s1.TRANSPORT)
+    trans_vol = data.get('trans_vol', s1.TRANSPORT_VOLUMES)
+    stor = data.get('stor', s1.STORAGE)
+    stor_vol = data.get('stor_vol', s1.STORAGE_VOLUMES)
     
-    results = {
-        s1.monte_carlo(n_reps = n_reps, 
+    results = s1.monte_carlo(n_reps = n_reps, 
                        base_rate = base_rate, 
                        replication_seed = replication_seed, 
                        clusters = clusters, 
@@ -34,8 +35,7 @@ def double_number():
                        stor_vol = stor_vol, 
                        frac_split = frac_split, 
                        sampling = sampling)
-    }
-
+    
     return jsonify(results)
 
 @app.route('/api/run-model')
