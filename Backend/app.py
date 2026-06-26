6581de3a75466eef2019cd4f0d22d65dbec7395d
Michael <mg6090@princeton.edu>
2026-06-26 12:42:00 -0400

    fixed backend connection

--- Backend/app.py
+++ Backend/app.py
@@ -7,42 +7,6 @@ app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/api/run-s1', methods=['GET', 'POST'])
-def run_s1():
-    data = request.get_json(silent = True) or {}
-    
-    n_reps = data.get('n_reps', 10)
-    sampling = data.get('sampling', True)
-    frac_split = data.get('frac_split', s1.FRAC_SPLIT)
-    base_rate = data.get('base_rate', s1.BASE_RATE)
-    replication_seed = data.get('replication_seed', 0)
-    clusters = data.get('clusters', s1.CLUSTERS)
-    caps = data.get('caps', s1.CAPTURE)
-    caps_vol = data.get('caps_vol', s1.CAPTURE_VOLUMES)
-    trans = data.get('trans', s1.TRANSPORT)
-    trans_vol = data.get('trans_vol', s1.TRANSPORT_VOLUMES)
-    stor = data.get('stor', s1.STORAGE)
-    stor_vol = data.get('stor_vol', s1.STORAGE_VOLUMES)
-    max_rate = data.get('s1_max_rate', s1.MAX_RATE)
-    cap_tolerance = data.get('s1_cap_tol', s1.CAPTURE_TOLERANCE)
-
-    results = s1.monte_carlo(n_reps = n_reps, 
-                       base_rate = base_rate, 
-                       max_rate = max_rate,
-                       cap_tolerance = cap_tolerance,
-                       replication_seed = replication_seed, 
-                       clusters = clusters, 
-                       caps = caps, 
-                       caps_vol = caps_vol, 
-                       trans = trans, 
-                       trans_vol = trans_vol, 
-                       stor = stor, 
-                       stor_vol = stor_vol, 
-                       frac_split = frac_split, 
-                       sampling = sampling)
-    
-    return jsonify(results)
-
 @app.route('/api/run-model', methods = ['GET', 'POST'])
 def run_model():
     data = request.get_json(silent = True) or {}
@@ -66,9 +30,13 @@ def run_model():
         s1_trans_vol = s1_params.get('trans_vol', s1.TRANSPORT_VOLUMES)
         s1_stor = s1_params.get('stor', s1.STORAGE)
         s1_stor_vol = s1_params.get('stor_vol', s1.STORAGE_VOLUMES)
+        max_rate = s1_params.get('s1_max_rate', s1.MAX_RATE)
+        cap_tolerance = s1_params.get('s1_cap_tol', s1.CAPTURE_TOLERANCE)
 
         s1_results = s1.monte_carlo(n_reps = s1_n_reps, 
                        base_rate = s1_base_rate, 
+                       max_rate = max_rate,
+                       cap_tolerance = cap_tolerance,
                        clusters = s1_clusters, 
                        caps = s1_caps, 
                        caps_vol = s1_caps_vol, 
